fb26d1797e04e5ed8392187f57c630f2c63d5437
shreejit07 <shreejit@simformsolutions.com>
2022-11-16 16:40:05 +0530

    First commit

--- K6script.js
+++ K6script.js
@@ -1,10 +1,19 @@
 import http from 'k6/http';
-import check from 'k6';
+import {check} from 'k6';
+
+export let options={
+ //stages:[
+  // {duration:'10s', target:5},
+  //  {duration:'20s', target:10}
+ // ]
+//vus: 10, duration:'10s'
+}
 
 export default function() {
-  let url = 'https://httpbin.test.k6.io/post';
-  let response = http.post(url, 'Hello world!');
-  check(response, {
-      'Application says hello': (r) => r.body.includes('Hello world!')
-  });
+ let response = http.get("https://run.mocky.io/v3/292bdbd2-590d-4b26-9b1e-288a34195a20");
+ check(response, {
+  'Response status is 200!' : (r)=> r.status ===200,
+})
+
 }
+
