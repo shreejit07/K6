051df8124cca29681cc1a2928f37762110fa6321
shreejit07 <shreejit@simformsolutions.com>
2022-11-18 10:46:32 +0530

    Added one more group for another API.

--- K6Auth.js
+++ K6Auth.js
@@ -37,6 +37,23 @@ export default (Token) => {
 	let operators = http.get(`https://tampdevapp.azurewebsites.net/v1/User/Operators`, {headers: request_headers});
 	const message = operators.json().result;
     console.log(message);
+    check(operators, {
+      'Response status is 200!': (r) => r.status === 200,
+    })
 	
-    });		
+    });	
+    group('Get Widgets for User', () => {
+      const request_headers = {
+		  'content-type': "application/json",
+		  'Authorization': `Bearer ${Token}`};
+	console.log("The Header: " + JSON.stringify(request_headers));
+	//Get widgets for Users
+	let res_widgets = http.get(`https://tampdevapp.azurewebsites.net/v1/Widget/GetWidgetsForUser`, {headers: request_headers});
+	const message = res_widgets.json().result;
+    console.log(message);
+    check(res_widgets, {
+      'Response status is 200!': (r) => r.status === 200,
+    })
+	
+    });	 	
 };
