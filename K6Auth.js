26143edd5c389dfa415b40e8b6a0440adf2d5163
shreejit07 <shreejit@simformsolutions.com>
2022-11-23 12:39:22 +0530

    Update existing files 23Nov

--- K6Auth.js
+++ K6Auth.js
@@ -9,7 +9,7 @@ export function setup() {
         email: 'joshray@yopmail.com',
         password: 'Shree@1234',
       });
-      
+
       const params = {
         headers: {
           'Content-Type': 'application/json',
@@ -22,38 +22,44 @@ export function setup() {
       'Response status is 200!' : (r)=> r.status ===200,
       })
       const Token = response.json('result.token');
-      console.log('Token is:'+ Token);    
+      //console.log('Token is:'+ Token);    
 
 	return Token;
 }
 
 export default (Token) => {
-	 group('Get Operators', () => {
-      const request_headers = {
-		  'content-type': "application/json",
-		  'Authorization': `Bearer ${Token}`};
-	console.log("The Header: " + JSON.stringify(request_headers));
-	//Get all Operators
-	let operators = http.get(`https://tampdevapp.azurewebsites.net/v1/User/Operators`, {headers: request_headers});
-	const message = operators.json().result;
-    console.log(message);
+
+  group('Get Operators', () => {
+    const request_headers = {
+      'content-type': "application/json",
+      'Authorization': `Bearer ${Token}`
+    };
+    //console.log("The Header: " + JSON.stringify(request_headers));
+    //Get all Operators
+    let operators = http.get(`https://tampdevapp.azurewebsites.net/v1/User/Operators`, { headers: request_headers });
+    const message = operators.json().result;
+    //console.log(message);
+    // let body = JSON.parse(operators.body);
+    // body.forEach(element=>{
+    //   console.log('Names of Operators ${element.name}')})c
     check(operators, {
       'Response status is 200!': (r) => r.status === 200,
     })
-	
-    });	
-    group('Get Widgets for User', () => {
-      const request_headers = {
-		  'content-type': "application/json",
-		  'Authorization': `Bearer ${Token}`};
-	console.log("The Header: " + JSON.stringify(request_headers));
-	//Get widgets for Users
-	let res_widgets = http.get(`https://tampdevapp.azurewebsites.net/v1/Widget/GetWidgetsForUser`, {headers: request_headers});
-	const message = res_widgets.json().result;
+  });
+
+  group('Get Widgets for User', () => {
+    const request_headers = {
+      'content-type': "application/json",
+      'Authorization': `Bearer ${Token}`
+    };
+    console.log("The Header: " + JSON.stringify(request_headers));
+    //Get widgets for User
+    let res_widgets = http.get(`https://tampdevapp.azurewebsites.net/v1/Widget/GetWidgetsForUser`, { headers: request_headers });
+    const message = res_widgets.json().result;
     console.log(message);
     check(res_widgets, {
       'Response status is 200!': (r) => r.status === 200,
     })
-	
-    });	 	
+
+  });
 };
---
+++ K6script(Trend&check).js
@@ -0,0 +1,42 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import {Trend} from 'k6/metrics';
+//import {rate} from 'k6/metrics';
+var getApitrend = new Trend("Get_api_trend");
+var postApitrend = new Trend("Post_api_trend");
+export let options = {
+  stages: [
+    { duration: '10s', target: 5 },
+    { duration: '10s', target: 10 }
+  ]
+  //vus: 10, duration:'10s'
+}
+
+export function setup() {
+//test
+
+}
+
+
+export default function () {
+  let response1 = http.get('http://test.k6.io'); 
+  getApitrend.add(response1.timings.duration);
+  const url = 'http://test.k6.io/login';
+  const payload = JSON.stringify({
+    email: 'aaa',
+    password: 'bbb',
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  let response2 = http.post(url, payload, params);
+  postApitrend.add(response1.timings.duration);
+  check(response1, {
+    'Response status is 200!' : (r)=> r.status ===200,
+    })
+}
+
